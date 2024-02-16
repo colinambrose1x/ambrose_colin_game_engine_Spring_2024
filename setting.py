ceb8e861402d15046c78a9a93ee66028ffcf979d
colinambrose1x <160263627+colinambrose1x@users.noreply.github.com>
2024-02-16 09:00:49 -0800

    Hello GitHub!

---
+++ setting.py
@@ -0,0 +1,13 @@
+# dimensions of the game
+WIDTH = 800
+HEIGHT = 600
+TILESIZE = 32
+PLAYER_SPEED = 300
+FPS = 30
+
+# colors for the game
+BGCOLOR = (0,0,0)
+GREEN = (0,255,0)
+YELLOW = (255,255,0)
+LIGHTGREY = (217,217,214)
+RED = (255, 0, 0)
