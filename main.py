d0f90831b39539511d5d84479b4f2d3c9fb383b3
colinambrose1x <160263627+colinambrose1x@users.noreply.github.com>
2024-02-28 10:00:19 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,21 +10,20 @@ from random import randint
 
 # create a game class 
 class Game:
-    # behold the methods...
+    # Define a special method to init the properties of said class...
     def __init__(self):
+        # init pygame
         pg.init()
-        # 
+        # set size of screen and be the screen
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        # 
-        pg.display.set_caption("My First Video Game")
-        # 
+        pg.display.set_caption(TITLE)
+        # setting game clock 
         self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100)
-        self.running = True
-        # later on we'll story game info with this
         self.load_data()
     def load_data(self):
         game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder, 'images')
+        self.player_img = pg.image.load(path.join(img_folder, 'Untitled.png')).comvert_alpha()
         self.map_data = []
         '''
         The with statement is a context manager in Python. 
@@ -35,83 +34,79 @@ class Game:
             for line in f:
                 print(line)
                 self.map_data.append(line)
-                print(self.map_data)
+    # Create run method which runs the whole GAME
     def new(self):
+        print("create new game...")
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        self.deathblock = pg.sprite.Group
-        # self.player = Player(self, 10, 10)
-        # self.all_sprites.add(self.player)
+        self.coins = pg.sprite.Group()
+        # self.player1 = Player(self, 1, 1)
         # for x in range(10, 20):
         #     Wall(self, x, 5)
         for row, tiles in enumerate(self.map_data):
             print(row)
             for col, tile in enumerate(tiles):
                 print(col)
-                if tile == 'x':
+                if tile == '1':
                     print("a wall at", row, col)
                     Wall(self, col, row)
-                if tile == 'p':
+                if tile == '':
                     self.player = Player(self, col, row)
-                if tile == 'd':
-                    Deathblock(self, col, row)
+                if tile == 'C':
+                    Coin(self, col, row)
+                if tile == 'p':
+                    #finds thepalyers starting coordinates
+                    self.p1col = col
+                    self.p1row = row
+                    self.p1 = Player(self, self.p1col, self.p1row)
 
     def run(self):
+        # 
         self.playing = True
         while self.playing:
             self.dt = self.clock.tick(FPS) / 1000
-            # this is input
             self.events()
-            # this is processing
             self.update()
-            # this output
             self.draw()
-
     def quit(self):
-        pg.quit()
-        sys.exit()
-    # methods
-    def input(self): 
-        pass
+         pg.quit()
+         sys.exit()
     def update(self):
         self.all_sprites.update()
     
     def draw_grid(self):
-        for x in range(0, WIDTH, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
-
+         for x in range(0, WIDTH, TILESIZE):
+              pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+         for y in range(0, HEIGHT, TILESIZE):
+              pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+    def draw_text(self, surface, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.topleft = (x*TILESIZE,y*TILESIZE)
+        surface.blit(text_surface, text_rect)
     def draw(self):
-        self.screen.fill(BGCOLOR)
-        self.draw_grid()
-        self.all_sprites.draw(self.screen)
-        pg.display.flip()
+            self.screen.fill(BGCOLOR)
+            self.draw_grid()
+            self.all_sprites.draw(self.screen)
+            self.draw_text(self.screen, str(self.player.moneybag), 64, WHITE, 1, 1)
+
+            pg.display.flip()
 
     def events(self):
-            # listening for events
-            for event in pg.event.get():
-                # when you hit the red x the window closes the game ends
-                if event.type == pg.QUIT:
-                    self.quit()
-                    print("the game has ended..")
-                # keyboard events
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_LEFT:
-                #         self.player.move(dx=-1)
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_RIGHT:
-                #         self.player.move(dx=1)
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_UP:
-                #         self.player.move(dy=-1)
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_DOWN:
-                #         self.player.move(dy=1)
-    def show_start_screen(self):
-        pass
-    def show_go_screen(self):
-        pass
+         for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            # if event.type == pg.KEYDOWN:
+            #     if event.key == pg.K_LEFT:
+            #         self.player.move(dx=-1)
+            #     if event.key == pg.K_RIGHT:
+            #         self.player.move(dx=1)
+            #     if event.key == pg.K_UP:
+            #         self.player.move(dy=-1)
+            #     if event.key == pg.K_DOWN:
+            #         self.player.move(dy=1)
 ####################### Instantiate game... ###################
 g = Game()
 # g.show_go_screen()
--- setting.py
+++ setting.py
@@ -1,9 +1,11 @@
 # dimensions of the game
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1024
+HEIGHT = 768
 TILESIZE = 32
 PLAYER_SPEED = 300
 FPS = 30
+
+TITLE = "My Fun Game"
 
 # colors for the game
 BGCOLOR = (0,0,0)
@@ -11,3 +13,6 @@ GREEN = (0,255,0)
 YELLOW = (255,255,0)
 LIGHTGREY = (217,217,214)
 RED = (255, 0, 0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
